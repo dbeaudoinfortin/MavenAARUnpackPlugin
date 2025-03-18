30a99d951a3bb4a780168c269acbd9b89f27fbe3
dbeaudoinfortin <david.fortin@8x8.com>
2025-03-17 21:18:09 -0400

    Automatically download the sources jar at the same time.

--- src/main/java/com/dbf/maven/aar/AarUnpackMojo.java
+++ src/main/java/com/dbf/maven/aar/AarUnpackMojo.java
@@ -1,9 +1,12 @@
 package com.dbf.maven.aar;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -88,7 +91,8 @@ public class AarUnpackMojo extends AbstractMojo {
 		List<RemoteRepository> mergedRepos = mergeRespositories();
 		
 		//Keep track of all of the classpath changes that we are making
-		Set<String> classpathChanges = new HashSet<String>();
+		//This is in the form of <Classes JAR, Sources JAR>
+		List<Map.Entry<String, String>> classpathChanges = new ArrayList<Map.Entry<String, String>>();
 		
 		boolean reloadDependencies = false;
 		if(aars == null || aars.isEmpty()) {
@@ -98,9 +102,11 @@ public class AarUnpackMojo extends AbstractMojo {
 				if (!"aar".equals(dependency.getType().toLowerCase())) continue;
 				
 				//Manually force the resolution of this artifact since Maven can't manually resolve it.
-				Artifact artifact = resolveAAR(new DefaultArtifact(dependency.getGroupId(), dependency.getArtifactId(), dependency.getClassifier(), "aar", dependency.getVersion()), mergedRepos);
+				Map.Entry<Artifact, Artifact> resolvedResult = resolveAAR(new DefaultArtifact(dependency.getGroupId(), dependency.getArtifactId(), dependency.getClassifier(), "aar", dependency.getVersion()), mergedRepos);
+				Artifact artifact = resolvedResult.getKey();
+				final Artifact sources = resolvedResult.getValue();
 				final String jarPath = artifact.getFile().getAbsolutePath();
-				classpathChanges.add(jarPath);
+				classpathChanges.add(new AbstractMap.SimpleEntry<String, String>(jarPath, sources == null ? null : sources.getFile().getAbsolutePath()));
 				
 				//Modify the dependency to make it a system look up
 				dependency.setScope("system");
@@ -121,9 +127,11 @@ public class AarUnpackMojo extends AbstractMojo {
 		        	Artifact artifact = new DefaultArtifact(aar);
 		        	
 		        	//Need to override the default extension of jar to aar
-		        	artifact = resolveAAR(new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(), artifact.getClassifier(), "aar", artifact.getVersion()), mergedRepos);
+		        	Map.Entry<Artifact, Artifact> resolvedResult =  resolveAAR(new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(), artifact.getClassifier(), "aar", artifact.getVersion()), mergedRepos);
+		        	artifact = resolvedResult.getKey();
+		        	final Artifact sources = resolvedResult.getValue();
 		        	final String jarPath = artifact.getFile().getAbsolutePath();
-					classpathChanges.add(jarPath);
+					classpathChanges.add(new AbstractMap.SimpleEntry<String, String>(jarPath, sources == null ? null : sources.getFile().getAbsolutePath()));
 					
 		        	//Need to add a new dependency to the main project
 		        	//This time it's a jar file making use of a system path
@@ -168,13 +176,14 @@ public class AarUnpackMojo extends AbstractMojo {
 	 * @return A resolved AAR artifact. May or may not exactly match the provided artifact.
 	 * @throws MojoExecutionException if the AAR artifact could not be resolved
 	 */
-	private Artifact resolveAAR(Artifact unresolvedArtifact, List<RemoteRepository> repos) throws MojoExecutionException {
+	private Map.Entry<Artifact, Artifact> resolveAAR(Artifact unresolvedArtifact, List<RemoteRepository> repos) throws MojoExecutionException {
 		final String aarFQN = convertToFQN(unresolvedArtifact);
 		getLog().info("Resolving AAR: " + aarFQN);
-		try {		
-			Artifact resolvedArtifact = resolveArtifact(unresolvedArtifact, repos);
-			final File jarFile = extractAAR(resolvedArtifact.getFile(), aarFQN);
-			return resolvedArtifact.setFile(jarFile);
+		try {
+			Map.Entry<Artifact, Artifact> resolvedResult = resolveArtifact(unresolvedArtifact, repos);
+			Artifact resolvedAARArtifact = resolvedResult.getKey();
+			final File jarFile = extractAAR(resolvedAARArtifact.getFile(), aarFQN);
+			return new AbstractMap.SimpleEntry<Artifact, Artifact>(resolvedAARArtifact.setFile(jarFile), resolvedResult.getValue());
 		} catch (Exception e) {
 			getLog().error("Error resolving AAR file: " + aarFQN + "\n" + StackTraceCompactor.getCompactStackTrace(e));
 			throw new MojoExecutionException("Error resolving AAR file: " + aarFQN, e);
@@ -196,30 +205,33 @@ public class AarUnpackMojo extends AbstractMojo {
 		if (!aarFile.exists()) {
             throw new MojoExecutionException("AAR file not found: " + aarFQN);
         }
-    	
+		
 		//Each AAR needs its own sub-directory because they all contain the same classes.jar file inside
     	File extractDir = new File(extractionDir + File.separator + aarFQN);
+    	
+    	final String aarAbsolutePath = aarFile.getAbsolutePath();
+    	final String extractDirAbsolutePath = extractDir.getAbsolutePath();
   
-    	getLog().info("Extracting AAR \"" + aarFile.getAbsolutePath()  + "\" to \"" + extractDir.getAbsolutePath() + "\".");
+    	getLog().info("Extracting AAR \"" + aarAbsolutePath  + "\" to \"" + extractDirAbsolutePath + "\".");
         try {
         	if(extractDir.exists()) {
         		if(extractDir.isDirectory()) {
         			MavenExecutionRequest request = session.getRequest();
         	        if(request.isUpdateSnapshots()) {
         	        	//Forcing the update of snapshots
-        	        	getLog().debug("Deleting directory " + extractDir.getAbsolutePath() + "\".");
+        	        	getLog().debug("Deleting directory \"" + extractDirAbsolutePath + "\".");
         	        	FileUtils.deleteDirectory(extractDir);
         	        	unZipAAR(aarFile, extractDir);
         	        }
         	        //Otherwise do nothing. Use the already extracted AAR
             	} else {
-            		getLog().warn("Conflicting file found at " + extractDir.getAbsolutePath() + "\".");
-            		getLog().debug("Deleting file " + extractDir.getAbsolutePath() + "\".");
+            		getLog().warn("Conflicting file found at \"" + extractDirAbsolutePath + "\".");
+            		getLog().debug("Deleting file \"" + extractDirAbsolutePath + "\".");
             		FileUtils.delete(extractDir);
             		unZipAAR(aarFile, extractDir);
             	}
         	} else {
-        		getLog().debug("Creating directory " + extractDir.getAbsolutePath() + "\".");
+        		getLog().debug("Creating directory \"" + extractDirAbsolutePath + "\".");
         		//Create a unique directory in the target directory
             	extractDir.mkdirs();
             	unZipAAR(aarFile, extractDir);
@@ -231,9 +243,23 @@ public class AarUnpackMojo extends AbstractMojo {
         //Check if the "classes.jar" file if present
         File classesJar = new File(extractDir + File.separator + "classes.jar");
         if(!classesJar.isFile()) {
-        	throw new MojoExecutionException("Missing 'classes.jar' in AAR: " + aarFQN);
+        	throw new MojoExecutionException("Missing \"classes.jar\" in AAR: " + aarFQN);
         }
         
+    	//Copy the sources JAR as well, if present
+        if(aarAbsolutePath.toLowerCase().endsWith(".aar")) { //Basic sanity check
+        	File sourcesJar = new File(aarAbsolutePath.substring(0, aarAbsolutePath.length()-4) + "-sources.jar");
+        	try {
+	        	if(sourcesJar.isFile()) {
+	        		getLog().debug("Copying sources JAR \"" + sourcesJar + "\" to directory \"" + extractDirAbsolutePath + "\".");
+	        		FileUtils.copyFileToDirectory(sourcesJar, extractDir);
+	        	}
+	        } catch (IOException e) {
+				getLog().debug("Error copying sources JAR \"" + sourcesJar + "\" to directory \"" + extractDirAbsolutePath + "\": + \n" + StackTraceCompactor.getCompactStackTrace(e));
+				//Don't throw an exception, source files are optional
+			}
+        }
+		
         //Return the location of the classes.jar file that was just extracted
         return classesJar;
 	}
@@ -246,13 +272,29 @@ public class AarUnpackMojo extends AbstractMojo {
         getLog().info("Successfully extracted AAR: " + aarFile.getAbsolutePath());
 	}
 	
-	private Artifact resolveArtifact(Artifact artifact, List<RemoteRepository> repos) throws ArtifactResolutionException {
+	private Map.Entry<Artifact, Artifact> resolveArtifact(Artifact artifact, List<RemoteRepository> repos) throws ArtifactResolutionException {
 		ArtifactRequest request = new ArtifactRequest();
 		request.setArtifact(artifact);
 		request.setRepositories(repos);
 
 		ArtifactResult result = repoSystem.resolveArtifact(repoSystemSession, request); //Always throws an exception if not found
-		return result.getArtifact();
+		
+		//Also attempt to automatically download the sources
+		ArtifactResult sourcesResult = null;
+		if(artifact.getClassifier() == null || !artifact.getClassifier().toLowerCase().equals("sources")) {
+			Artifact sourcesArtifact = new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(), "sources", "jar", artifact.getVersion());
+
+			try {
+				ArtifactRequest sourcesRequest = new ArtifactRequest();
+				sourcesRequest.setArtifact(sourcesArtifact);
+				sourcesRequest.setRepositories(repos);
+				sourcesResult = repoSystem.resolveArtifact(repoSystemSession, sourcesRequest);
+			} catch (ArtifactResolutionException e) {
+				getLog().debug("Unable to resolve sources JAR " + convertToFQN(sourcesArtifact) + "\n" + StackTraceCompactor.getCompactStackTrace(e));
+				//Don't throw an exception, source files are optional
+			}
+		}
+		return new AbstractMap.SimpleEntry<Artifact, Artifact>(result.getArtifact(), sourcesResult == null ? null : sourcesResult.getArtifact());
 	}
 	
 	private List<RemoteRepository> mergeRespositories() throws MojoExecutionException {
